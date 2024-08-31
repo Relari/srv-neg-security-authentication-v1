06f86fdd533adb3b132af6a1a2e7b10eea60d26c
Relari <renzolavador@gmail.com>
2024-08-30 19:47:00 -0500

    Se elimina el endpoint que valida al usuario

--- src/main/java/com/pe/relari/controller/UserController.java
+++ src/main/java/com/pe/relari/controller/UserController.java
@@ -1,14 +1,13 @@
 package com.pe.relari.controller;
 
 import com.pe.relari.security.model.api.UserRequest;
-import com.pe.relari.security.model.api.UserResponse;
-import com.pe.relari.security.service.JwtService;
 import com.pe.relari.security.service.UserService;
-import com.pe.relari.security.util.Utility;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
@@ -18,30 +17,12 @@ import javax.validation.Valid;
 public class UserController {
 
     private UserService service;
-    private JwtService jwtService;
 
     @PostMapping
     public ResponseEntity<Void> addNewUser(
             @RequestBody @Valid UserRequest userRequest) {
         service.addUser(userRequest);
         return ResponseEntity.noContent().build();
-    }
-
-    @GetMapping(path = "/{id}/validate")
-    public ResponseEntity<UserResponse> extractUser(
-            @RequestHeader(name = "Authorization") String token) {
-
-        String value = Utility.getTokenFromHeader(token);
-
-        if (value == null) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).build();
-        }
-
-        return ResponseEntity.ok(
-                new UserResponse(
-                        jwtService.extractUsername(value)
-                )
-        );
     }
 
 }
